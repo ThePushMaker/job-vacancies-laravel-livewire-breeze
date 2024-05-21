d93a39d9ff612cdac5afe4789f9b4fa613d6199c
Martin Calderón <elmcwl@gmail.com>
2024-05-20 22:52:23 -0700

    feat: add translation to the error messages to spanish

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,9 +1,9 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
+    <ul {{ $attributes->merge(['class' => 'mt-3 list-none text-sm space-y-2']) }}>
         @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+            <li class="bg-red-100 border-l-4 border-red-600 text-red-600 font-bold p-3">{{ $message }}</li>
         @endforeach
     </ul>
 @endif
