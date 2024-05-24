b5a56972fd5277dbc3d27a5b488cceb3a1c0ec13
Martin Calderón <elmcwl@gmail.com>
2024-05-23 21:48:40 -0700

    feat: add link component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,16 +32,22 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+        <div class="flex justify-between my-5">
+            <x-link
+                :href="route('register')"
+                Crear Cuenta
+            >
+            </x-link>
+        
+            <x-link
+                :href="route('password.request')"
+            >
+               ¿Olvidaste tu contraseña?
+            </x-link>
         </div>
+        
+        <x-primary-button class="ms-3">
+            {{ __('Log in') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
---
+++ resources/views/components/link.blade.php
@@ -0,0 +1,7 @@
+@php
+    $classes = "hover:underline text-sm text-gray-500 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</a>
