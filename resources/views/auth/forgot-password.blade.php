4099eae4fc08cfc859f8edc6349eca5fad221709
Martin Calderón <elmcwl@gmail.com>
2024-05-23 22:23:18 -0700

    feat: changes to forgot password component

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('¿Olvidaste tu contraseña? Coloca tu correo de registro y enviaremos un enlace para qeu pueda crear uno nuevo.') }}
     </div>
 
     <!-- Session Status -->
@@ -11,15 +11,27 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Correo')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="flex justify-between my-5">
+            <x-link
+                :href="route('login')"
+            >
+                Inciar Sesión
+            </x-link>
+        
+            <x-link
+                :href="route('register')"
+            >
+               Crear Cuenta
+            </x-link>
         </div>
+        
+        <x-primary-button class="w-full justify-center">
+            {{ __('Enviar Instrucciones') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
