fd50622c1a0085b129a7e1c7c29fdc604cc4f8be
Martin Calderón <elmcwl@gmail.com>
2024-05-24 21:28:05 -0700

    style: add translation to the email confirmation page

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,11 +1,11 @@
     <x-guest-layout>
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+            {{ __('Es necesario confirmar tu cuenta antes de continuar, revissa tu correo y presiona sobre el enlace de verificación.') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
             <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                {{ __('Hemos enviado un nuevo correo de confirmación a la cuenta que colocaste en el registro.') }}
             </div>
         @endif
 
@@ -15,7 +15,7 @@
 
                 <div>
                     <x-primary-button>
-                        {{ __('Resend Verification Email') }}
+                        {{ __('Reenviar correo de verificación') }}
                     </x-primary-button>
                 </div>
             </form>
@@ -23,8 +23,11 @@
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
 
-                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    {{ __('Log Out') }}
+                <button type="submit" class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <x-link
+                    >
+                        {{ __('Cerrar sesión') }}
+                    </x-link>
                 </button>
             </form>
         </div>
