32c7e80dff3ff058dd2269b402b42f932d25651f
Martin Calderón <elmcwl@gmail.com>
2024-05-25 16:21:42 -0700

    feat: add customization to the email sent to the user to verify their account

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verificar Cuenta')
+                ->line('Tu Cuenta ya está casi lista, solo debes presionar el enlace a continuación.')
+                ->action('Verificar Cuenta', $url)
+                ->line('Si no creaste esta cuenta, puedes ignorar este correo.');
+        });
     }
 }
