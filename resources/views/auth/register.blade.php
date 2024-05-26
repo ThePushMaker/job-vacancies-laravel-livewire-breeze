16c579b527c3c48ed008b66620af45cc227c5a3b
Martin Calderón <elmcwl@gmail.com>
2024-05-25 18:21:40 -0700

    feat: add migration to store user's rol

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,9 +23,10 @@
                 id="rol"
                 name="rol"
                 class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+            >
                 <option value="">-- Selecciona un rol --</option>
-                <option value="1">-- Developer - Obtener Empleo --</option>
-                <option value="2">-- Recruiter - Publicar Empleos --</option>
+                <option value="1">Developer - Obtener Empleo</option>
+                <option value="2">Recruiter - Publicar Empleos</option>
             >
             </select>
         </div>
