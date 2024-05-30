efe3c65dc01bfd10082a9c550585766ee1b50ee0
Martin Calderón <elmcwl@gmail.com>
2024-05-29 17:25:47 -0700

    feat: add form to publish a vacancie

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,3 +1,78 @@
-<div>
-    <h2>Desde livewire</h2>
-</div>
+<form class="md:w-1/2 space-y-5">
+    <div>
+        <x-input-label for="titulo" :value="__('Titulo Vacante')" />
+        <x-text-input 
+            id="titulo" 
+            class="block mt-1 w-full" 
+            type="text" 
+            name="titulo" 
+            :value="old('titulo')"
+            placeholder="Ej. Programador Laravel Full Stack"
+        />
+    </div>
+    
+    <div>
+        <x-input-label for="salario" :value="__('Salario Mensual')" />
+        <select
+            id="salario"
+            name="salario"
+            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+        >
+        </select>
+    </div>
+    
+    <div>
+        <x-input-label for="categoria" :value="__('Categoría')" />
+        <select
+            id="categoria"
+            name="categoria"
+            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+        >
+        </select>
+    </div>
+    
+    <div>
+        <x-input-label for="empresa" :value="__('Empresa')" />
+        <x-text-input 
+            id="empresa" 
+            class="block mt-1 w-full" 
+            type="text" 
+            name="empresa" 
+            :value="old('empresa')"
+            placeholder="Ej. Netflix, Uber, Shopify"
+        />
+    </div>
+    
+    <div>
+        <x-input-label for="ultimo_dia" :value="__('Último día para postularse')" />
+        <x-text-input 
+            id="ultimo_dia" 
+            class="block mt-1 w-full" 
+            type="date" 
+            name="ultimo_dia" 
+            :value="old('ultimo_dia')"
+        />
+    </div>
+    
+    <div>
+        <x-input-label for="descripcion" :value="__('Descripción del Puesto')" />
+        <textarea
+            name="descripcion"
+            placeholder="Ej. Descripción general del Puesto, experiencia"
+            class="rounded-md border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50 w-full h-72"
+        ></textarea>
+    </div>
+    
+    <div>
+        <x-input-label for="imagen" :value="__('Imagen')" />
+        <x-text-input 
+            id="imagen" 
+            class="block mt-1 w-full" 
+            type="file" 
+        />
+    </div>
+    
+    <x-primary-button class="w-full justify-center">
+        Crear Vacante
+    </x-primary-button>
+</form>
--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -9,7 +9,7 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 text-gray-900">
-                <h2 class="text-2xl font-bold text-center mb-10">Publicar Vacante</h2>
+                <h2 class="text-2xl font-bold text-center my-10">Publicar Vacante</h2>
                 
                 <div class="md:flex md:justify-center p-5">
                       <livewire:crear-vacante />
