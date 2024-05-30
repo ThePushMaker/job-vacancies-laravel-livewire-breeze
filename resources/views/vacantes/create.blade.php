5c55393983f5d762e0e0f06385e123ef5ddbd002
Martin Calderón <elmcwl@gmail.com>
2024-05-29 17:01:00 -0700

    feat: add create-vacancie livewire component

--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -9,8 +9,12 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 text-gray-900">
-                  Formulario Aquí
-              </div>
+                <h2 class="text-2xl font-bold text-center mb-10">Publicar Vacante</h2>
+                
+                <div class="md:flex md:justify-center p-5">
+                      <livewire:crear-vacante />
+                </div>
+            </div>
           </div>
       </div>
   </div>
