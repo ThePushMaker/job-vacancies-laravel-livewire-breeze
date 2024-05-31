9d5a4ed1ec4ef6c6e1d3b3caa785fb61204a0dd6
Martin Calderón <elmcwl@gmail.com>
2024-05-31 00:19:58 -0700

    feat: add salaries seed

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Salario;
 use Livewire\Component;
 
 class CrearVacante extends Component
 {
     public function render()
     {
-        return view('livewire.crear-vacante');
+        // consultar DB
+        $salarios = Salario::all('nombre', 'id');
+        
+        return view('livewire.crear-vacante', [
+            'salario' => $salarios
+        ]);
     }
 }
