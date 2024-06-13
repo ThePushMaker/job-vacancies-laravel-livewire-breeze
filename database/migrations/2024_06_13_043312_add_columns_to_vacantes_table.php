7d115d1bd6890991e7638a43f1831dd1f734aa82
Martin Calderón <elmcwl@gmail.com>
2024-06-12 22:22:34 -0700

    feat: add dropForeign and dropColumn to vacancies migration

--- database/migrations/2024_06_13_043312_add_columns_to_vacantes_table.php
+++ database/migrations/2024_06_13_043312_add_columns_to_vacantes_table.php
@@ -30,6 +30,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('vacantes', function (Blueprint $table) {
+            $table->dropForeign('vacantes_user_id_foreign');
+            $table->dropForeign('vacantes_salario_id_foreign');
+            $table->dropForeign('vacantes_categoria_id_foreign');
             $table->dropColumn(['titulo', 'salario_id', 'categoria_id', 'empresa', 'ultimo_dia', 'descripcion', 'imagen', 'publicado', 'user_id']);
         });
     }
