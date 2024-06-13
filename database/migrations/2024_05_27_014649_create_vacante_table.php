ba5ecb38ddff06eb3e2e0e8c978c0f03b8e1123a
Martin Calderón <elmcwl@gmail.com>
2024-06-12 21:20:43 -0700

    feat: add fields to vancies table

--- database/migrations/2024_05_27_014649_create_vacante_table.php
+++ database/migrations/2024_05_27_014649_create_vacante_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('vacantes', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->foreignId('salario_id')->constrained()->onUpdate('set null')->onDelete('set null');
             $table->timestamps();
         });
     }
