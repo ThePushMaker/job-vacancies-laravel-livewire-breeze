f04a517a1d9062d69fca6edc705e573365c111cf
Martin Calderón <elmcwl@gmail.com>
2024-06-16 22:10:37 -0700

    feat: add pagination to vacancies

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,33 +1,40 @@
-<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-    @forelse ($vacantes as $vacante)
-        <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
-            <div class="space-y-3">
-                <a href="#" class="text-xl font-bold">
-                    {{ $vacante->titulo }}
-                </a>
-                <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>
-                <p class="text-sm text-gray-500">Último día: {{ $vacante->ultimo_dia->format('d/m/Y') }}</p>
-            </div>
-            
-            <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
-                <a 
-                    href="#"
-                    class="bg-slate-700 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center"
-                >Candidatos</a>
+
+<div>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        @forelse ($vacantes as $vacante)
+            <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
+                <div class="space-y-3">
+                    <a href="#" class="text-xl font-bold">
+                        {{ $vacante->titulo }}
+                    </a>
+                    <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>
+                    <p class="text-sm text-gray-500">Último día: {{ $vacante->ultimo_dia->format('d/m/Y') }}</p>
+                </div>
                 
-                <a 
-                    href="#"
-                    class="bg-blue-500 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center"
-                >Editar</a>
-                
-                <a
-                    href="#"
-                    class="bg-red-500 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center"
-                >Eliminar</a>
+                <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
+                    <a 
+                        href="#"
+                        class="bg-slate-700 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center"
+                    >Candidatos</a>
+                    
+                    <a 
+                        href="#"
+                        class="bg-blue-500 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center"
+                    >Editar</a>
+                    
+                    <a
+                        href="#"
+                        class="bg-red-500 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center"
+                    >Eliminar</a>
+                </div>
             </div>
-        </div>
-    @empty
-        <p class="p-3 text-center text-sm text-gray-600">No hay vacantes que mostrar</p>
-    @endforelse
-    
+        @empty
+            <p class="p-3 text-center text-sm text-gray-600">No hay vacantes que mostrar</p>
+        @endforelse
+    </div>
+
+    {{-- Paginacion de Vacantes --}}
+    <div class="mt-10">
+        {{ $vacantes->links() }}
+    </div>
 </div>
