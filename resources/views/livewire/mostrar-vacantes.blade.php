904bfbd0986aa999fb9a53782441143f8e7c3bd8
Martin Calderón <elmcwl@gmail.com>
2024-06-16 21:01:53 -0700

    feat: add a message to display to the user when there are no vacancies

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-    @foreach ($vacantes as $vacante)
+    @forelse ($vacantes as $vacante)
         <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
             <div class="space-y-3">
                 <a href="#" class="text-xl font-bold">
@@ -20,11 +20,14 @@
                     class="bg-blue-500 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center"
                 >Editar</a>
                 
-                <a 
+                <a
                     href="#"
                     class="bg-red-500 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center"
                 >Eliminar</a>
             </div>
         </div>
-    @endforeach
+    @empty
+        <p class="p-3 text-center text-sm text-gray-600">No hay vacantes que mostrar</p>
+    @endforelse
+    
 </div>
