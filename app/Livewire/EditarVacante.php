c91734c6ac0d4ec88bfb4d3d1a3aa05b9bcb486e
Martin Calderón <elmcwl@gmail.com>
2024-06-19 19:40:51 -0700

    feat: add  functionality to autofill form with information of the vacancy to edit

--- app/Livewire/EditarVacante.php
+++ app/Livewire/EditarVacante.php
@@ -6,15 +6,29 @@ use App\Models\Categoria;
 use App\Models\Salario;
 use App\Models\Vacante;
 use Database\Seeders\CategoriasSeeder;
+use Illuminate\Support\Carbon;
 use Livewire\Component;
 
 class EditarVacante extends Component
 {
     public $titulo;
+    public $salario;
+    public $categoria;
+    public $empresa;
+    public $ultimo_dia;
+    public $descripcion;
+    public $imagen;
     
     public function mount(Vacante $vacante)
     {
         $this->titulo = $vacante->titulo;
+        $this->salario = $vacante->salario_id;
+        $this->categoria = $vacante->categoria_id;
+        $this->empresa = $vacante->empresa;
+        $this->ultimo_dia = Carbon::parse( $vacante->ultimo_dia)->format('Y-m-d') ;
+        $this->descripcion = $vacante->descripcion;
+        $this->imagen = $vacante->imagen;
+        
     }
     
     public function render()
--- resources/views/livewire/editar-vacante.blade.php
+++ resources/views/livewire/editar-vacante.blade.php
@@ -116,12 +116,20 @@
             @endif
         </div> --}}
         
+        <div class="my-5 w-96">
+            <x-input-label :value="__('Imagen Actual')" />
+            <img 
+                src="{{ asset('storage/vacantes/' . $imagen) }}" 
+                alt={{ 'Imagen Vacante ' . $titulo}} 
+            />
+        </div>
+        
         @error('imagen')
             <livewire:mostrar-alerta :message="$message" />
         @enderror
     </div>
     
     <x-primary-button class="w-full justify-center">
-        Crear Vacante
+        Guardar Cambios
     </x-primary-button>
 </form>
