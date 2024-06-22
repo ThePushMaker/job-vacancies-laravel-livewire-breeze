154e78142f1ae07b7e8e4ca0ab915a13616a8ffc
Martin Calderón <elmcwl@gmail.com>
2024-06-21 20:40:03 -0700

    feat: add show vacancy view

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -4,7 +4,7 @@
         @forelse ($vacantes as $vacante)
             <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
                 <div class="space-y-3">
-                    <a href="#" class="text-xl font-bold">
+                    <a href="{{  route('vacantes.show', $vacante->id) }}" class="text-xl font-bold">
                         {{ $vacante->titulo }}
                     </a>
                     <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>
