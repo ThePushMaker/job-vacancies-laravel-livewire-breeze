c9b7c1b6e269d6e582f600f2c00e34587961894c
Martin Calderón <elmcwl@gmail.com>
2024-06-23 12:33:10 -0700

    feat: add input to upload cv

--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <p>Aquí se mostrará el form</p>
+<div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
+    <h3 class="text-center text-2xl font-bold my-4">Postularme a esta vacante</h3>
+    
+    <form action="" class="w-96 mt-5">
+        <div class="mb-4">
+            <x-input-label for="cv" :value="__('Curriculum o Hoja de Vida (PDF)')"/>
+            <x-text-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full"/>
+        </div>
+    </form>
 </div>
