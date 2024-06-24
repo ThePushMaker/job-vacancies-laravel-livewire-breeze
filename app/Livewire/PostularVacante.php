cf32c049b34cfec0b5395400e204df623d0d186b
Martin Calderón <elmcwl@gmail.com>
2024-06-23 21:29:28 -0700

    feat: add function to store the cv

--- app/Livewire/PostularVacante.php
+++ app/Livewire/PostularVacante.php
@@ -3,9 +3,32 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class PostularVacante extends Component
 {
+    use WithFileUploads;
+    public $cv;
+    
+    protected $rules = [
+        'cv' => 'required|mimes:pdf'
+    ];
+    
+    public function postularme()
+    {
+        $datos = $this->validate();
+        
+        // almacenar la imagen cv
+        $cv = $this->cv->store('public/cv');
+        $datos['cv'] = str_replace('public/cv/', '', $cv);
+        
+        // crear la vacante
+        
+        // crear la notificación y enviar email
+        
+        // mostrar al ususario mensaje de ok
+    }
+    
     public function render()
     {
         return view('livewire.postular-vacante');
