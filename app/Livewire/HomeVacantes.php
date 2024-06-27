0f27009f8ff1ce56ce4c03d7258ee3da6eb82770
Martin Calderón <elmcwl@gmail.com>
2024-06-27 01:16:15 -0700

    feat: add function to filter vacancies using eloquent

--- app/Livewire/HomeVacantes.php
+++ app/Livewire/HomeVacantes.php
@@ -24,7 +24,11 @@ class HomeVacantes extends Component
     
     public function render()
     {
-        $vacantes = Vacante::all();
+        // $vacantes = Vacante::all();
+        // encuentra el termino en el titulo de las vacantes
+        $vacantes = Vacante::when($this->termino, function($query) {
+            $query->where('titulo', 'LIKE', '%' . $this->termino . '%'); 
+        })->paginate(20);
         
         return view('livewire.home-vacantes', [
             'vacantes' => $vacantes
