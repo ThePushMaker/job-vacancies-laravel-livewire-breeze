aaffc96fd951b912dc81dfcdf9ebe451ad66a863
Martin Calderón <elmcwl@gmail.com>
2024-06-26 21:43:04 -0700

    feat: add home page

---
+++ app/Livewire/HomeVacantes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class HomeVacantes extends Component
+{
+    public function render()
+    {
+        return view('livewire.home-vacantes');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
 use App\Http\Controllers\CandidatosController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NotificacionController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VacanteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', HomeController::class)->name('home');
 
 Route::get('/dashboard', [VacanteController::class, 'index'])->middleware(['auth', 'verified', 'rol.reclutador'])->name('vacantes.index');
 Route::get('/vacantes/create', [VacanteController::class, 'create'])->middleware(['auth', 'verified'])->name('vacantes.create');
